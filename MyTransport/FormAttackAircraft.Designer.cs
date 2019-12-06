ba4d94917cce104518f3bca954e00f0e99116696
KhafizovaEI-PIbd22 <elena150620@gmail.com>
2019-12-06 20:17:00 +0400

    Лабораторная № 1

---
+++ MyTransport/FormAttackAircraft.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MyTransport
+{
+    partial class FormAttackAircraft
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAttackAircraft));
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.pictureBoxAttackAircraft = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAttackAircraft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(722, 359);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(28, 27);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(680, 392);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(31, 23);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(722, 415);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(28, 23);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(757, 392);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(31, 23);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxAttackAircraft
+            // 
+            this.pictureBoxAttackAircraft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAttackAircraft.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxAttackAircraft.Name = "pictureBoxAttackAircraft";
+            this.pictureBoxAttackAircraft.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxAttackAircraft.TabIndex = 5;
+            this.pictureBoxAttackAircraft.TabStop = false;
+            this.pictureBoxAttackAircraft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormAttackAircraft
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBoxAttackAircraft);
+            this.Name = "FormAttackAircraft";
+            this.Text = "FormAttackAircraft";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAttackAircraft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.PictureBox pictureBoxAttackAircraft;
+    }
+}
+
---
+++ MyTransport/FormAttackAircraft.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTransport
+{
+    public partial class FormAttackAircraft : Form
+    {
+
+        private AttackAircraft aircraft;
+        public FormAttackAircraft()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxAttackAircraft.Width, pictureBoxAttackAircraft.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            aircraft.DrawAttackAircraft(gr);
+            pictureBoxAttackAircraft.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            aircraft = new AttackAircraft(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Gray, Color.Blue, true, true);
+            aircraft.SetPosition(rnd.Next(100, 300), rnd.Next(100, 300), pictureBoxAttackAircraft.Width, pictureBoxAttackAircraft.Height);
+            Draw();
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    aircraft.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    aircraft.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    aircraft.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    aircraft.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+    }
+}
